d7e387beff8aa090a76e74e5a6c4768b83b4c6f3
aptealan <aptealan@gmail.com>
2020-09-06 17:26:37 -0700

    Included Player vs Computer option

--- DiceGame/app.js
+++ DiceGame/app.js
@@ -8,13 +8,15 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, gamePlaying, winScore;
+var scores, roundScore, activePlayer, gamePlaying, winScore, pvp, rollNums;
 
+pvp = true;
 init();
 
 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
+    console.log("roll");
     if(gamePlaying) {
         var dice = Math.floor(Math.random() * 6) + 1;
         var diceDOM = document.querySelector('.dice');
@@ -47,7 +49,7 @@ document.querySelector('.winning-score').addEventListener('keypress', function(e
 })
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-
+    console.log("hold");
     if(gamePlaying) {
         scores[activePlayer] += roundScore;
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
@@ -73,16 +75,108 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     } 
 });
 
+document.querySelector('.btn-pvp').addEventListener('click', function() {
+    pvp = true;
+    init();
+});
+
+document.querySelector('.btn-pvc').addEventListener('click', function() {
+    pvp = false;
+    init();
+});
+
 function nextPlayer() {
+    console.log("nextPlayer");
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
+
+    if(activePlayer === 1 && pvp === false) {
+        //document.getElementById('active .player-name::after').style.right = "-65px";
+        //console.log(document.getElementById('.active .player-name::after').style.right);
+        //document.querySelector('.player-name').style.fontSize= '30px'
+        var compFont = document.getElementById('name-1');
+        compFont.style.fontSize = "32px"; 
+        runComputer();
+    }
+    else {
+        var compFont = document.getElementById('name-1');
+        compFont.style.fontSize = "40px"; 
+    }
+}
+
+function runComputer() {
+    console.log("runComputer");
+    rollNums = 0;
+    while(true) {
+        if(activePlayer != 1) {
+            break;
+        }
+        /*
+        if(activeScore[1] == 0) {
+            /*
+            setTimeout(computerRoll(), 1000);
+            if(activePlayer != 1) {
+                break;
+            }
+            setTimeout(computerHold(), 2000);
+            break;
+            //
+            var dice = Math.floor(Math.random() * 6) + 2;
+            var diceDOM = document.querySelector('.dice');
+
+            diceDOM.style.display = 'block';
+            diceDOM.src = 'dice-' + dice + '.png';
+            activeScore[1] = dice;
+            nextPlayer();
+            break;
+        }
+        */
+        if(rollNums > 4) {
+            setTimeout(computerHold(), 2000);
+            break;
+        }
+
+        setTimeout(computerRoll(), 1000);
+        rollNums++;
+
+        if(activePlayer != 1) {
+            break;
+        }
+        if(rollNums < 3 && (document.getElementById('current-' + activePlayer).textContent + scores[activePlayer]) >= 90) {
+            while(rollNums < 3) {
+                setTimeout(computerRoll(), 1000);
+                rollNums++;
+                if(activePlayer != 1) {
+                    break;
+                }
+            }
+        }
+        else if(rollNums === 3 && document.getElementById('current-' + activePlayer).textContent >= 10) {
+            setTimeout(computerRoll(), 1000);
+            rollNums++;
+            if(activePlayer != 1) {
+                break;
+            }
+            setTimeout(computerHold(), 2000);
+            break;
+        }
+    }
+}
+
+function computerRoll() {
+    document.getElementById('rollBtn').click();
+}
+
+function computerHold() {
+    document.getElementById('holdBtn').click();
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
 function init() {
+    console.log("init");
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
@@ -96,7 +190,13 @@ function init() {
     document.getElementById('current-1').textContent = '0';
 
     document.querySelector('#name-0').textContent = 'Player 1';
-    document.querySelector('#name-1').textContent = 'Player 2';
+    if(pvp === true) {
+        document.querySelector('#name-1').textContent = 'Player 2';
+    }
+    else {
+        document.querySelector('#name-1').textContent = 'Computer';
+    }
+    
 
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
